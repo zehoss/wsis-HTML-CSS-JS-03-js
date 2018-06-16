5d69bd5d565af20e27828eaa2c6fb0b5d61e7118
zehoss <zehoss@gmail.com>
2018-06-16 18:13:18 +0200

    Smooth scrolling

--- public_html/js/scroll.js
+++ public_html/js/scroll.js
@@ -2,7 +2,7 @@ var headerTitleLink = document.querySelector('.header-title a');
 
 headerTitleLink.addEventListener('click', function (event) {
 
-    document.querySelector('.brown-section').scrollIntoView();
+    document.querySelector('.brown-section').scrollIntoView({behavior: "smooth"});
 
     event.preventDefault();
 });
